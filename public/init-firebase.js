c3df05684582cb53fc94acd753e4f875ddacbcd1
Edd <eddie.gracilla@collabera.com>
2022-12-17 20:55:19 +0800

    upd

--- public/init-firebase.js
+++ public/init-firebase.js
@@ -1,6 +1,14 @@
 
 
 var firebaseConfig = {
+  apiKey: "AIzaSyDkebw2fUeNaOVwdExsECQMtfB8s-v2bK4",
+  authDomain: "eureka-apps-000.firebaseapp.com",
+  databaseURL: "https://eureka-apps-000.firebaseio.com",
+  projectId: "eureka-apps-000",
+  storageBucket: "eureka-apps-000.appspot.com",
+  messagingSenderId: "897804371731",
+  appId: "1:897804371731:web:f756ca081654d895f7fce6",
+  measurementId: "G-6E3TKFEKT4"
 }
 
 // Initialize Firebase
@@ -11,7 +19,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   // This gives you a Facebook Access Token. You can use it to access the Facebook API.
   var token = result.credential.accessToken;
   // The signed-in user info.
-  
+  // console.log(JSON.stringify(result.user))
   var user = JSON.parse(JSON.stringify(result.user))
 
   console.log(`Bearer ${user.stsTokenManager.accessToken}`)
